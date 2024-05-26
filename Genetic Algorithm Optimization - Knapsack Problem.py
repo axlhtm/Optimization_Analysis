a0ab0e4ca2c6cb6ec7e880ad8067b9b85d49419b
Ir. Ignasius Axel Hutomo, S.T. M.Sc., IPP <axelhutomo@gmail.com>
2024-05-26 22:22:23 +0700

    Modified version of GA - Knapsack (Single Objective)

--- Genetic Algorithm Optimization - Knapsack Problem.py
+++ Genetic Algorithm Optimization - Knapsack Problem.py
@@ -5,6 +5,7 @@
 # Import Python Library 
 import numpy as np
 import os
+import pandas as pd
 import pygad
 
 # Problem Context 
@@ -26,9 +27,19 @@ Genetic Algorithm Simmulation Approach:
 os.chdir("C:/Users/axelh/OneDrive/Documents/Hydroinformatics/Optimization_Analysis/")
 
 # Import Datasets
-weights     = np.array([3, 4, 5, 8, 10, 20, 2, 2, 3])           # Weigth of each item (kg)
-values      = np.array([10, 40, 30, 15, 20, 30, 10, 5, 10])     # Value of each item 
-capacity    = 15                                                # Maximum bag capacity (kg)
+weights     = np.array([3, 4, 5, 8, 10, 20, 2, 2, 3, 8])           # Weigth of each item (kg)
+values      = np.array([10, 40, 30, 15, 20, 30, 10, 15, 10, 10])   # Value of each item 
+fragility   = np.array([2, 1, 3, 4, 1, 2, 5, 1, 2, 5])             # Fragility of each item
+capacity    = 15                                                   # Maximum bag capacity (kg)
+risk        = 12                                                   # Maximum risk tolerance
+
+# Create a Dataframe 
+data = {
+    "Weight"    : weights,
+    "Value"     : values,
+    "Fragility" : fragility
+}
+df = pd.DataFrame(data, index=["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"])
 
 # Check Total Weights 
 total_weight = np.sum(weights)
@@ -39,11 +50,26 @@ else:
 
 # Defining the Objective Functions of GA
 def fitness_func(ga_instance, solution, solution_idx):
-    # Check for capacity violation
-    if np.sum(weights * solution) > capacity:
-        return 0  # Penalty for exceeding capacity
-    # Calculate total value
-    return np.sum(values * solution)
+    '''
+    The objective functions for the knapsack problem will follow this rule: 
+        1. Item 2 and 10 must be brought
+        2. The maximum weight value <= the capacity value
+        3. The maximum fragility value <= the risk value
+        4. Reach maximum value 
+    '''
+    ## Force inclusion of item 2 (index 1) and item 10 (index 9) by setting its solution value to 1
+    solution[1] = 1
+    solution[9] = 1
+    ## Check for capacity condition
+    if np.sum(weights * solution) <= capacity:
+        ## Check for risk condition
+        if np.sum(fragility * solution) <= risk: 
+            ## Check for value condition
+            return np.sum(values * solution)
+        else: 
+            return 0
+    else: 
+        return 0
 
 # Initialising Genetic Algorithm
 ga_instance = pygad.GA(num_generations          = 30,               # Number of generations
@@ -68,6 +94,18 @@ ga_instance.run()
 # Access Results 
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
 print("Best Solution:", solution)
-print("Number of items that can be bought is", sum(solution), " items.")
-print("Total Weight:", np.sum(weights * solution))
-print("Total Value:", np.sum(values * solution))
+print("Number of items that can be bought is", sum(solution), "items.")
+print("Total weight of the selected items:", np.sum(weights * solution), "kg.")
+print("Total fragility risk:", np.sum(fragility * solution))
+print("Total value of the selected items:", np.sum(values * solution)) 
+
+# Find the Selected Item Names
+item_names = df.index.tolist()
+selected_items = []  # List to store names of selected items
+for i in range(len(solution)):
+  if solution[i] == 1:
+    selected_items.append(item_names[i])
+print(f"You need to bring {len(selected_items)} items, which are: {', '.join(selected_items)}.")
+
+
+
